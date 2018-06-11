b9cea9d5d7af34abcc460cf8aab5bfe895290703
wenhan <mecera@aliyun.com>
2018-06-11 18:58:03 +0800

    第二次

---
+++ dmeo/src/cn/demo/Demo.java
@@ -0,0 +1,13 @@
+package cn.demo;
+
+/**
+ * 仅仅是个Demo
+* @author wenhan
+* @ClassName : typename∗@Version版本∗@ModifiedBy修改人
+* @Copyright公司名称∗@date2018年6月11日 下午6:52:58
+ */
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("你好这个世界");
+	}
+}
